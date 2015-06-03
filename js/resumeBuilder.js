b0cd139c03e97e5f7a4c73a7feb6ea95efe1d4c1
Brian Chen <bchen.chen93@gmail.com>
2015-06-03 18:53:03 +1200

    feat add vars needed for my resume

---
+++ js/resumeBuilder.js
@@ -0,0 +1,65 @@
+var bio = {
+  name: "Brian Chen",
+  role: "Front-end Web Developer",
+  contacts: {
+    mobile: "0211742289",
+    email: "bchen.chen93@gmail.com",
+    github: "https://github.com/Brianzchen/",
+    twitter: "@brianzchen",
+    location: "Auckland, New Zealand"
+  },
+  welcomeMessage: "Welcome to my online resume",
+  skills: {"html" },
+  biopicL: "images/fry.jpg"
+}
+
+var education = {
+  schools: [
+    {
+      name: "University of Auckland"
+      location: "Auckland, New Zealand",
+      degree: "Bachelor of Science",
+      majors: {"Compsci", "Statistics"},
+      dates: 2015,
+      url: "www.auckland.ac.nz"
+    }
+  ]
+  onlineCourses: [
+    {
+      title: "Front-end Web Developer Nanodegree",
+      school: "Udacity",
+      date: 2015,
+      url: "www.udacity.com"
+    }
+  ]
+}
+
+var work = {
+  jobs: [
+    {
+      employer: "Skycity Entertainment Group",
+      title: "Croupier",
+      location: "Auckland, New Zealand",
+      dates: "November 2013 - March 2015",
+      description: "Blackjack dealer, winner of 2014 dealers championship"
+    },
+    {
+      employer: "Fortune Enterprises Ltd",
+      title: "Account Manager",
+      location: "Auckland, New Zealand",
+      dates: "March 2015 - Present",
+      description: "Manager of company accounts and daily activities. Took initiative to modernize company through moving company files to a cloud based system and modularized the daily activities of the director"
+    }
+  ]
+}
+
+var projects = {
+  projects: [
+    {
+      title: "Classic Arcade Clone",
+      dates: "June 2015 - Present",
+      description: "I made a game, will say more later",
+      images: {"images/197x148.gif"}
+    }
+  ]
+}
