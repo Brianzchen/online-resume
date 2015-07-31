189b746e79c83c620333c7781797aae86a87d3d1
Brian Chen <bchen.chen93@gmail.com>
2015-07-31 13:01:50 +1200

    feat: Updated skills section to include newly learnt frameworks

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -9,7 +9,7 @@ var bio = {
   },
   "welcomeMessage": "Welcome to my online resume",
   "skills": [
-    "HTML5", "CSS3", "JavaScript"
+    "HTML5", "CSS3", "JavaScript", "Knockout.js", "Jasmine 2.0"
     ],
   "biopic": "images/fry.jpg"
 };
